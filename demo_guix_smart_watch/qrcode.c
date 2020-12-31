72917b6fc60adee0e188c213909ae40205d9f960
wtcat <wt1454246140@163.com>
2020-12-31 17:11:03 +0800

    Updated

--- demo_guix_smart_watch/qrcode.c
+++ demo_guix_smart_watch/qrcode.c
@@ -22,33 +22,45 @@ static void printQr(const uint8_t qrcode[])
 
 static void guix_show_qr(GX_WIDGET *widget, const uint8_t *qrcode)
 {
-	int size = qrcodegen_getSize(qrcode);
-	GX_RECTANGLE rect = widget->gx_widget_size;
-	GX_RECTANGLE fill_rect;
-	int border = 4;
+	int qr_size = qrcodegen_getSize(qrcode);
+	GX_RECTANGLE fill_rect, rect = widget->gx_widget_size;
+	const int pixel_dot_size = 4;
+	int x_border, y_border;
+	int x_center, y_center;
+	int x_origin;
 
 	rect.gx_rectangle_top += 50;
 	fill_rect = rect;
-	gx_context_brush_define(GX_COLOR_ID_WHITE, GX_COLOR_ID_WHITE, GX_BRUSH_SOLID_FILL | GX_BRUSH_ROUND);
+
 	gx_context_brush_width_set(0);
+	gx_context_brush_define(GX_COLOR_ID_WHITE, 
+							GX_COLOR_ID_WHITE, 
+							GX_BRUSH_SOLID_FILL | GX_BRUSH_ROUND);
 	gx_canvas_rectangle_draw(&rect);
 
-	gx_context_brush_define(GX_COLOR_ID_BLACK, GX_COLOR_ID_BLACK, GX_BRUSH_SOLID_FILL | GX_BRUSH_ROUND);
-	gx_context_brush_width_set(4);
-	for (int y = -border; y < size + border; y++) {
-		for (int x = -border; x < size + border; x++) {
+	gx_context_brush_define(GX_COLOR_ID_BLACK, 
+							GX_COLOR_ID_BLACK, 
+							GX_BRUSH_SOLID_FILL | GX_BRUSH_ROUND);
+
+	x_center = (rect.gx_rectangle_right - rect.gx_rectangle_left + 1) / 2;
+	y_center = (rect.gx_rectangle_bottom - rect.gx_rectangle_top + 1) / 2;
+	x_border = x_center / pixel_dot_size - (qr_size / 2);
+	y_border = y_center / pixel_dot_size - (qr_size / 2);
+	x_origin = rect.gx_rectangle_left;
+	fill_rect.gx_rectangle_left = x_origin;
+
+	for (int y = -y_border; y < qr_size; y++) {
+		for (int x = -x_border; x < qr_size; x++) {
 			if (qrcodegen_getModule(qrcode, x, y)) {
-				fill_rect.gx_rectangle_right = fill_rect.gx_rectangle_left + 3;
-				fill_rect.gx_rectangle_bottom = fill_rect.gx_rectangle_top + 3;
+				fill_rect.gx_rectangle_right = fill_rect.gx_rectangle_left + (pixel_dot_size - 1);
+				fill_rect.gx_rectangle_bottom = fill_rect.gx_rectangle_top + (pixel_dot_size - 1);
 				gx_canvas_rectangle_draw(&fill_rect);
-				//_gx_canvas_line_draw(point.gx_point_x, point.gx_point_y, point.gx_point_x + 1, point.gx_point_y + 1);
-				//gx_canvas_pixel_draw(point);
 			}
-			fill_rect.gx_rectangle_left += 4;
+			fill_rect.gx_rectangle_left += pixel_dot_size;
 		}
 
-		fill_rect.gx_rectangle_left = rect.gx_rectangle_left;
-		fill_rect.gx_rectangle_top += 4;
+		fill_rect.gx_rectangle_left = x_origin;
+		fill_rect.gx_rectangle_top += pixel_dot_size;
 	}
 }
 
