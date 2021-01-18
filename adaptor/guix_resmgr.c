0d8405ef99947211ec01a3daf9a07c5c864668bc
wtcat <wt1454246140@163.com>
2021-01-18 12:42:21 +0800

    Updated resource node

--- adaptor/guix_resmgr.c
+++ adaptor/guix_resmgr.c
@@ -2,56 +2,37 @@
 #include <stdio.h>
 
 #include "guix_resmgr.h"
-#include "list.h"
-
-
-struct res_node {
-	struct list_head node;
-	void (*destory)(struct res_node* res);
-	uint8_t data[];
-};
+#include "resource_base.h"
 
 static LIST_HEAD(res_root);
-
-static void free_node(struct res_node* res)
+static void free_node(struct resource_node* res)
 {
 	if (res)
 		free(res);
 }
 
-void res_free(void* ptr)
-{
-	struct res_node* res;
-	if (!ptr)
-		return;
-
-	res = container_of(ptr, struct res_node, data);
-	list_del(&res->node);
-	res->destory(res);
-}
-
 void *res_alloc(size_t size)
 {
-	struct res_node* res;
-	res = calloc(1, sizeof(struct res_node) + size);
-	if (!res) {
+	void* ptr = resource_alloc(&res_root, malloc, 
+		free, size);
+	if (!ptr) {
 		printf("No more memory\n");
 		return NULL;
 	}
+	return ptr;
+}
 
-	res->destory = free_node;
-	list_add(&res->node, &res_root);
-	return res->data;
+void res_free(void* ptr)
+{
+	struct resource_node* res;
+	if (!ptr)
+		return;
+
+	res = container_of(ptr, struct resource_node, data);
+	resource_release(res);
 }
 
 void res_free_all(void)
 {
-	struct list_head *pos, *next;
-	struct res_node *curr;
-
-	list_for_each_safe(pos, next, &res_root) {
-		curr = container_of(pos, struct res_node, node);
-		list_del(pos);
-		curr->destory(curr);
-	}
+	resources_release(&res_root);
 }
